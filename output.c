19b92f36bd109733843ef36c7ffdfd8ff16d6ade
erezphilosof <95741723+erezphilosof@users.noreply.github.com>
2025-08-05 12:09:14 +0300

    Include addresses in object file

--- output.c
+++ output.c
@@ -7,7 +7,8 @@ bool write_object_file(const char *filename,
                        const uint16_t *instructions,
                        int instruction_count,
                        const uint16_t *data,
-                       int data_count)
+                       int data_count,
+                       int base_address)
 {
     FILE *f = fopen(filename, "w");
     if (!f) { perror("open .ob"); return false; }
@@ -15,17 +16,20 @@ bool write_object_file(const char *filename,
     // שורה ראשונה: מספר הוראות ומספר מילים בקובץ נתונים
     fprintf(f, "%d %d\n", instruction_count, data_count);
 
-    // הוראות מקודדות
-    for (int i = 0; i < instruction_count; i++) {
-        char buf[32];
-        convert_to_base4(instructions[i], buf);
-        fprintf(f, "%s\n", buf);
+    // הוראות מקודדות עם כתובות
+    int address = base_address;
+    for (int i = 0; i < instruction_count; i++, address++) {
+        char addr_buf[32], word_buf[32];
+        convert_to_base4((uint16_t)address, addr_buf);
+        convert_to_base4(instructions[i], word_buf);
+        fprintf(f, "%s %s\n", addr_buf, word_buf);
     }
     // ואחריהן קטע הנתונים
-    for (int i = 0; i < data_count; i++) {
-        char buf[32];
-        convert_to_base4(data[i], buf);
-        fprintf(f, "%s\n", buf);
+    for (int i = 0; i < data_count; i++, address++) {
+        char addr_buf[32], word_buf[32];
+        convert_to_base4((uint16_t)address, addr_buf);
+        convert_to_base4(data[i], word_buf);
+        fprintf(f, "%s %s\n", addr_buf, word_buf);
     }
     fclose(f);
     return true;
